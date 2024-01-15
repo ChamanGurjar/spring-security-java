3a0d3a75d95aaf67a7e1318d2e5d04afdcee09b2
Chaman Gurjar <chamangurjar@Chamans-MacBook-Air.local>
2024-01-15 08:36:05 +0530

    Database communication with postgresql setup.

---
+++ src/main/java/com/gurjar/chaman/springsecurity/entities/Users.java
@@ -0,0 +1,24 @@
+package com.gurjar.chaman.springsecurity.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+}
